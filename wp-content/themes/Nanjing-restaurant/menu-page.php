5309963543d0f4f3a8bc1dc4ba462174fead4ed8
namwoody <deepsarora262@gmail.com>
2017-02-28 07:46:36 -0500

    done

--- wp-content/themes/Nanjing-restaurant/menu-page.php
+++ wp-content/themes/Nanjing-restaurant/menu-page.php
@@ -18,7 +18,7 @@
 
 
 	<div id="menu-content">
-		
+		<div class="container">
 		
 		  <div id="tabs">
 
@@ -41,7 +41,7 @@
 				</li>
 
 			  </ul>
-  			<div id="tabs-1">
+  		<div id="tabs-1" >
   				<h2>Lunch</h2>
 				  <span> MON-FRI 11am-3pm</span>
 				    <p>Served with choice of Veg/Chicken Soup
@@ -49,72 +49,90 @@
 				Choice of One Entree with White Rice or Brown Rice</p>
 				   <h2>Entree</h2>
 				   <h3>Vegetables</h3>
-<p>
-<ul>
-<li>Chili Paneer </li>
-<li>Nanjing Vegetables</li>
-<li>Vegetable Manchurian </li>
-<li>Sweet &amp; Sour Vegetables</li>
-<li>Eggplant (Hot Garlic or Szechwan sauce)</li>
-<li>Chili Cauliflower</li>
-<li>Thai Vegetable Curry (red, green, yellow or panang)</li>
-<li>Sarson Ka Saag </li>
-<li>Alu Palak </li>
-<li>Alu Matar/Mushroom Matar</li>
-<li>Alu Gobi/Gobi Masala</li>
-<li>Baingan Bartha</li>
-<li>Chana Masala </li>
-<li>Bhindi Masala</li>
-<li>Malai Kofta</li>
-<li>Kadai Paneer Masala</li>
-<li>Palak Paneer</li>
-<li>Dal Makhani</li>
-<li>Yellow Dal</li>
-<li>Vegetable Jalfrazi</li>
-</ul>
-</p>
-  </div>
+			<p>
+			<ul>
+			<li>Chili Paneer </li>
+			<li>Nanjing Vegetables</li>
+			<li>Vegetable Manchurian </li>
+			<li>Sweet &amp; Sour Vegetables</li>
+			<li>Eggplant (Hot Garlic or Szechwan sauce)</li>
+			<li>Chili Cauliflower</li>
+			<li>Thai Vegetable Curry (red, green, yellow or panang)</li>
+			<li>Sarson Ka Saag </li>
+			<li>Alu Palak </li>
+			<li>Alu Matar/Mushroom Matar</li>
+			<li>Alu Gobi/Gobi Masala</li>
+			<li>Baingan Bartha</li>
+			<li>Chana Masala </li>
+			<li>Bhindi Masala</li>
+			<li>Malai Kofta</li>
+			<li>Kadai Paneer Masala</li>
+			<li>Palak Paneer</li>
+			<li>Dal Makhani</li>
+			<li>Yellow Dal</li>
+			<li>Vegetable Jalfrazi</li>
+			</ul>
+			</p>
+  		</div>
   <!-- Start Appetizers -->
   <div id="tabs-2">
-  <h2>Appetizers - Vegetarian</h2>
- <span class="menu-item">Chilli Paneer (Dry)  8</span>
-<i>Cubes of Cheese Sautéed with fresh chili &amp; onions in light soya sauce.</i>
-<span class="menu-item">Crispy Lotus (Dry) 8</span>
-<i>Crispy lotus stems stir fried with onions in a honey sauce.</i>
-<span class="menu-item">Tofu Delight  8</span>
-<i>Crispy tofu glazed with sweet chili sauce.</i>
-<span class="menu-item">Vegetarian Manchuria (Dry) 8</span>
-<i>Golden fried vegetable dumplings tossed with onions in a chili garlic sauce</i>	
-<span class="menu-item">Cauliflower Manchurian (Dry) . . . . . . . . . . . . . . . . . . . . . . . . . 8</span>
-<i>Crispy Bettered Cauliflower tossed with onions &amp; scallions in a chili sauce.</i>
-<span class="menu-item">Samosas . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 6</span>
-<i>Puff pastry with a savory filling such as spiced potatoes, onions, peas and lentils</i>
-<span class="menu-item">Aloo Papdi Chaat . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 7</span>
-<i>A mixture of wheat crisps, potatoes and chickpeas in yogurt and tamarind sauce.</i>
-<span class="menu-item">Paneer Pakora . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 7</span>
-<i>Homemade cottage cheese cubes dipped in gram flour and fried.</i>
-<span class="menu-item">Samosa Chaat . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 8</span>
-<i>Served with chick peas, yogurt, tamarind and cilantro chutneys.</i>
-<span class="menu-item">Ragda Patties. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 8</span>
-<i>Potato patties seasoned with herbs and served on a bed of spiced chick peas.</i>
-<span class="menu-item">Soyabean Kebab . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 9</span>
-<i>Shallow fried Soybean chunks mixed with bean curd, tapioca &amp; herbs spices.</i>
-<span class="menu-item">Palak Kebab . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 9</span>
-<i>Lentil and spinach cooked together and served with mint &amp; tamarind sauce.</i>
-<span class="menu-item">Nanjing Combo Platter Vegetables . . . . . . . . . . . . . . . . . . . . 22</span>
-<i>Spring rolls, veg. Manchurian, Panner Pakora &amp; Samosa.</i>
+    
+    <div id="appetizers">
+		  <h2>Appetizers - Vegetarian</h2>
+		 <span class="menu-item">Chilli Paneer (Dry)  8</span>
+		<i>Cubes of Cheese Sautéed with fresh chili &amp; onions in light soya sauce.</i>
+		<span class="menu-item">Crispy Lotus (Dry) 8</span>
+		<i>Crispy lotus stems stir fried with onions in a honey sauce.</i>
+		<span class="menu-item">Tofu Delight  8</span>
+		<i>Crispy tofu glazed with sweet chili sauce.</i>
+		<span class="menu-item">Vegetarian Manchuria (Dry) 8</span>
+		<i>Golden fried vegetable dumplings tossed with onions in a chili garlic sauce</i>	
+		<span class="menu-item">Cauliflower Manchurian (Dry)  8</span>
+		<i>Crispy Bettered Cauliflower tossed with onions &amp; scallions in a chili sauce.</i>
+		<span class="menu-item">Samosas 6</span>
+		<i>Puff pastry with a savory filling such as spiced potatoes, onions, peas and lentils</i>
+		<span class="menu-item">Aloo Papdi Chaat 7</span>
+		<i>A mixture of wheat crisps, potatoes and chickpeas in yogurt and tamarind sauce.</i>
+		<span class="menu-item">Paneer Pakora 7</span>
+		<i>Homemade cottage cheese cubes dipped in gram flour and fried.</i>
+		<span class="menu-item">Samosa Chaat 8</span>
+		<i>Served with chick peas, yogurt, tamarind and cilantro chutneys.</i>
+		<span class="menu-item">Ragda Patties  8</span>
+		<i>Potato patties seasoned with herbs and served on a bed of spiced chick peas.</i>
+		<span class="menu-item">Soyabean Kebab  9</span>
+		<i>Shallow fried Soybean chunks mixed with bean curd, tapioca &amp; herbs spices.</i>
+		<span class="menu-item">Palak Kebab  9</span>
+		<i>Lentil and spinach cooked together and served with mint &amp; tamarind sauce.</i>
+		<span class="menu-item">Nanjing Combo Platter Vegetables  22</span>
+		<i>Spring rolls, veg. Manchurian, Panner Pakora &amp; Samosa.</i>
 
+
+	</div>
+
+	<div id="appetizers">
+      <h2>Appetizers - Non - Vegetarian</h2>
+       <span class="menu-item">Dumpling </span>
+        <i>Steamed or fried (veg 7  Chicken 8  Shrimp 9)</i>
+		<span class="menu-item">Spring Roll Fried (veg 7  Chicken 8 )</span>
+		<span class="menu-item">Crispy Chicken with Honey and Dried Chilies 8</span>
+		<i>sauteed in chili honey sauce.</i>
+		<span class="menu-item">Chicken Satee  9</span>
+		<i>Chicken on Skewer delicately cooked on griddle served with peanut sauce.</i>
+		<span class="menu-item">Nanjing Heaven  11 </span>
+		<i>Drums of heaven with chilies and garlic, wok fried in chili sauce.</i>
+		<span class="menu-item">Grilled Tilapia 12 </span>
+		<i>Served with hot chili garlic sauce </i> 
+		<span class="menu-item">Grilled Garlic Shrimp 12</span>
+		<i>Marinated chili and garlic shrimps served on a skewer.</i>
+		<span class="menu-item">Salt &amp; Pepper Calamari  12</span>
+		<i>Garlic, Ginger, Peppers, onions &amp; cilantro in black pepper with salt.</i>
+		<span class="menu-item">Nanjing Combo Platter Non-vegetarian 29</span>
+		<i>Lamb chops, chicken satee, Malai kabab, kabab, fish Tikka.</i>
+	</div>
 
 
   </div>
 
-  <!-- End Appetizers -->
-  <div id="tabs-10">
-
-  </div>
-  <div id="tabs-12">
-
-  </div>
 
 
 
@@ -124,6 +142,9 @@
 
 
 		
+	</div><!-- end ui tab -->
+
+
 	</div>
 </div>
 
